96fed2e7d5487eee45a25eb9cd79038b9ec2b086
Osvaldo Colina <osvaldo.colina@gmail.com>
2019-10-05 14:58:48 -0300

    Fix issue with echo_messages

--- src/routes/messenger.js
+++ src/routes/messenger.js
@@ -1,8 +1,8 @@
 const zurbo = require('../bot/zurbo');
 
 module.exports = (app) => {
-  app.get('/messenger/webhook', async(req, res) => {
-    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN){
+  app.get('/messenger/webhook', async (req, res) => {
+    if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
       res.send(req.query['hub.challenge']);
     } else {
       res.send('Invalid verify token');
@@ -12,9 +12,12 @@ module.exports = (app) => {
   app.post('/messenger/webhook', async (req, res) => {
     res.sendStatus(200);
     let messagingEvents = req.body.entry[0].messaging;
-    for (let i = 0; i < messagingEvents.length; i++){
+    for (let i = 0; i < messagingEvents.length; i++) {
       const event = messagingEvents[i];
-      if (event.message && (event.message.text || event.message.sticker_id) && !event.message.quick_reply) {
+      if (event.message
+        && (event.message.text || event.message.sticker_id)
+        && !event.message.quick_reply
+        && !event.message.is_echo) {
         await zurbo.processFreeText(event);
       }
     }
